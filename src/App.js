caa762e7fbfc1a1eb7db437ab139c0f6d6bc0f30
Kleber Lima <kchikui@hotmail.com>
2023-01-24 19:26:48 +0900

    Step 03

--- src/App.js
+++ src/App.js
@@ -7,17 +7,6 @@ function App() {
       <header className="App-header">
         <h1>Kleber Chikui Lima</h1>
         <h3>WebDev</h3>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
